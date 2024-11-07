39b72a26cf6159c0e74c246ae8de578aa873037e
Ankithakumaraswamy <ankithakumaraswamy@gmail.com>
2024-11-07 16:23:32 +0530

    recurssive function & pointers

--- DAY3/pat1.c
+++ DAY3/pat1.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    int n=0;
+    int i,j,n;
     scanf("%d",&n);
-    for(int i=n;i>0;i--)
+    for(int i=0;i<n;i++);
     {
-        for(int j=0;j>0;j--)
-            printf("%d ",j+1);
+        for(int j=n;j>i;j--)
+            printf("%d ",j);
         printf("\n");
     }
     return 0;
